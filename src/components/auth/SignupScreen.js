9734424a7221fa66b3e810f94305d88814e4ebb5
MarcoCiau <marcoantoniociau@hotmail.com>
2022-03-07 17:59:17 -0600

    signin & signup html

--- src/components/auth/SignupScreen.js
+++ src/components/auth/SignupScreen.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
 
 export const SignupScreen = () => {
   return (
-    <div>SignupScreen</div>
-  )
-}
+    <>
+      <h1>Create an account</h1>
+      <p>
+        Already have an account? <a href="/signin">Sign In Here</a>{" "}
+      </p>
+
+      <form>
+        <div class="form-group">
+          <label for="registerEmail">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="registerEmail"
+            placeholder="Enter email"
+            name="email"
+          />
+        </div>
+        <div class="form-group">
+          <label for="registerPassword">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="registerPassword"
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <br />
+        <button type="submit" class="btn btn-primary btn-lg btn-block">
+          Sign Up
+        </button>
+        <button class="btn btn-danger btn-lg btn-block">
+          <i className="fab fa-google"> Sign Up with Google </i>
+        </button>
+      </form>
+    </>
+  );
+};
