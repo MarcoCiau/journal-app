3f2f72f43b22a0d47cfb9fa92d085a5138905059
MarcoCiau <marcoantoniociau@hotmail.com>
2022-03-08 06:55:40 -0600

    sidebar with entries styled

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -2,10 +2,27 @@ import React from "react";
 
 export const JournalEntry = () => {
   return (
-    <div className="journal__entry">
+    <div className="journal__entry util-pointer">
+      <div
+        className="journal__entry-picture"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage:
+            "url(https://vivaldi.com/wp-content/uploads/note-taking-apps-980x551.png)",
+        }}
+      ></div>
 
-        <div className="journal__entry-picture"></div>
+      <div className="journal__entry-body">
+        <p className="journal__entry-body_title">un nuevo día</p>
+        <p className="journal__entry-body_content">
+          asjdnsjdsd sdjkdsidsjsdjsddddddddddddjsdjds
+        </p>
+      </div>
+
+      <div className="journal__entry-date-box">
+        <span>Monday</span>
+        <h2>28</h2>
+      </div>
     </div>
-  
   );
 };
